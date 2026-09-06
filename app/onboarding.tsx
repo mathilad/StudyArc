456ef4e4839dba53385776c34fa6e5b0c79132ff
mathilad <mathiladinimuthu2@gmail.com>
2026-09-06 14:17:56 +0530

    Use browser-safe icons after onboarding

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,4 +1,4 @@
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons } from "../components/SafeIcons";
 import { LinearGradient } from "expo-linear-gradient";
 import { Redirect, useRouter } from "expo-router";
 import React, { useMemo, useState } from "react";
