ce2c57da9548deea1c8230ef720a97c2f027b05b
mathilad <mathiladinimuthu2@gmail.com>
2026-09-14 07:04:33 +0530

    Expose assignments to timetable planner

--- screens/PlanWithAssignments.tsx
+++ screens/PlanWithAssignments.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import AssignmentTimetableStrip from "../components/AssignmentTimetableStrip";
+import { useAcademic } from "../context/AcademicContext";
+import { setRuntimeAssignments } from "../lib/timetableRuntime";
 import PlanScreen from "./PlanScreen";
 
 export default function PlanWithAssignments() {
+  const { assignments } = useAcademic();
+  setRuntimeAssignments(assignments);
   return <View style={s.root}><AssignmentTimetableStrip /><View style={s.plan}><PlanScreen /></View></View>;
 }
 const s = StyleSheet.create({ root:{flex:1,backgroundColor:"#080D14"}, plan:{flex:1} });
